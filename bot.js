01eb4de256db40f77848aecbb531b63e420c4aee
Joe X! <jamedinao@gmail.com>
2022-11-20 23:56:58 +0000

    Guard position wen spawned Send chat when spawned attack radius lowered to 4 blocks commented out chat event code to change it

---
+++ bot.js
@@ -0,0 +1,102 @@
+/**
+ * This bot example shows the basic usage of the mineflayer-pvp plugin for guarding a defined area from nearby mobs.
+ */
+
+const mineflayer = require('mineflayer')
+const { pathfinder, Movements, goals } = require('mineflayer-pathfinder')
+const pvp = require('mineflayer-pvp').plugin
+
+const bot = mineflayer.createBot({
+  host: process.env['host'],
+  port: parseInt(process.env['port']),
+  //auth: 'microsoft',
+  //username: process.env['email'],
+  username: process.env['username'],
+})
+
+bot.loadPlugin(pathfinder)
+bot.loadPlugin(pvp)
+
+let guardPos = null
+let spawned = false;
+
+bot.on('kicked', console.log)
+bot.on('error', console.error)
+
+// Assign the given location to be guarded
+function guardArea(pos) {
+  guardPos = pos
+
+  // We we are not currently in combat, move to the guard pos
+  if (!bot.pvp.target) {
+    moveToGuardPos()
+  }
+}
+
+// Cancel all pathfinder and combat
+function stopGuarding() {
+  guardPos = null
+  bot.pvp.stop()
+  bot.pathfinder.setGoal(null)
+}
+
+// Pathfinder to the guard position
+function moveToGuardPos() {
+  bot.pathfinder.setMovements(new Movements(bot))
+  bot.pathfinder.setGoal(new goals.GoalBlock(guardPos.x, guardPos.y, guardPos.z))
+}
+
+bot.on('spawn', () => {
+  const player = bot.player
+  if (!spawned) {
+    console.log(player.entity.position)
+    guardArea(player.entity.position)
+    setTimeout(() => bot.chat("beep boop beep boop"), 1000)
+  }
+  spawned = true;
+})
+
+// Called when the bot has killed it's target.
+bot.on('stoppedAttacking', () => {
+  if (guardPos) {
+    moveToGuardPos()
+  }
+})
+
+// Check for new enemies to attack
+bot.on('physicsTick', () => {
+  if (!guardPos) return // Do nothing if bot is not guarding anything
+
+  // Only look for mobs within 4 blocks
+  const filter = e => e.type === 'mob' && e.position.distanceTo(bot.entity.position) < 4 &&
+    e.mobType !== 'Armor Stand' // Mojang classifies armor stands as mobs for some reason?
+
+  const entity = bot.nearestEntity(filter)
+  console.log(entity)
+  if (entity) {
+    // Start attacking
+    bot.pvp.attack(entity)
+  }
+})
+
+// Listen for player commands
+bot.on('chat', (username, message) => {
+  /* Guard the location the player is standing
+  if (message === 'guard') {
+    const player = bot.players[username]
+
+    if (!player) {
+      bot.chat("I can't see you.")
+      return
+    }
+
+    bot.chat('I will guard that location.')
+    guardArea(player.entity.position)
+  }
+
+  // Stop guarding
+  if (message === 'stop') {
+    bot.chat('I will no longer guard this area.')
+    stopGuarding()
+  } */
+})
