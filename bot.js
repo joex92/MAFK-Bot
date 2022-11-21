87524d6a4f593b1732c5e0f3f820442a2fd4080c
Joe X! <jamedinao@gmail.com>
2022-11-21 14:05:51 +0000

    logging to file `.log`

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ const mineflayer = require('mineflayer')
 const { pathfinder, Movements, goals } = require('mineflayer-pathfinder')
 const pvp = require('mineflayer-pvp').plugin
 
+fs.writeFileSync( '.log', '' )
+
 function initBot() {
   const bot = mineflayer.createBot({
     host: process.env['host'],
@@ -10,13 +12,23 @@ function initBot() {
     auth: 'microsoft',
     username: process.env['email'],
     //username: process.env['username'],
+    //password: process.env['password']
   })
+
+  function logMsg(msg){
+    fs.appendFileSync('.log', msg.toString(), 'utf-8');
+  }
+
+  function obj2str(obj) {
+    return JSON.stringify(obj).replaceAll('[','[\t').replaceAll(']','\t]').replaceAll('{','{\t').replaceAll('}','\t}\n').replaceAll(':',': ').replaceAll(',',',\t').replaceAll('\n,',',')
+  }
+  
   bot.on('error', (err)=>{
     console.error(err)
-    logs.push(`ERROR: ${err.errno}
-    ${err.code}
-    ${err.syscall}`)
-    saveLogs()
+    let e = ''
+    Object.keys(err).forEach((v,i,a)=>{e+=`\t${v}: ${err[v]}\n`})
+    logs.push(`Error:\n${e}`)
+    logMsg(logs[logs.length-1])
   })
   bot.on('login', console.log)
 
@@ -27,13 +39,6 @@ function initBot() {
   let spawned = false
   let afk = false
   let logs = []
-
-  function saveLogs(){
-    fs.writeFile('.log', logs.join('\n'), function (err) {
-      if (err) return console.log(err);
-      console.log('Logs saved.');
-    });
-  }
   
   // Assign the given location to be guarded
   function guardArea(pos) {
@@ -55,11 +60,14 @@ function initBot() {
 
   bot.on('kicked', (reason, loggedIn) => {
     console.log('reason', reason)
+    let r = ''
+    Object.keys(reason).forEach((v,i,a)=>{r+=`\t${v}: ${reason[v]}\n`})
+    logs.push('Kicked:\n'+r)
     if (loggedIn) {
-      if (reason.text && reason.text.match(/[Kk][Ii][Cc][Kk]/)) {
+      if (r.match(/[Kk][Ii][Cc][Kk]/)) {
         bot.end()
         bot.quit()
-        saveLogs()
+        logMsg(logs[logs.length-1])
         proccess.exit()
         return
       }
@@ -68,7 +76,7 @@ function initBot() {
   })
   bot.on('death', (d) => {
     console.log('death', d);
-    //bot.end()
+    logs.push('Death:\t'+obj2str(bot.player.entity.position))
   })
   bot.on('end', () => setTimeout(() => initBot(), 5000))
   bot.on('respawn', () => guardArea(bot.player.entity.position))
@@ -81,10 +89,11 @@ function initBot() {
   bot.on('spawn', () => {
     if (!spawned) {
       guardArea(bot.player.entity.position)
-      setTimeout(() => bot.chat("hi, I'm _AFKing_  :upside_down:"), 500)
+      setTimeout(() => bot.chat("hi, I'm _AFKing_ :upside_down:"), 500)
     }
     spawned = true;
     setTimeout(() => afk = true, 5000)
+    console.log(bot.player)
   })
 
   // Called when the bot has killed it's target.
@@ -134,8 +143,9 @@ function initBot() {
       bot.chat('I will no longer guard this area.')
       stopGuarding()
     } */
-    console.log(message,jsonMsg)
-    logs.push(`${jsonMsg.translate}: ${message}`)
+    console.log(message,obj2str(jsonMsg))
+    logs.push(`${jsonMsg.translate ? jsonMsg.translate : 'message'}: ${message}\n`)
+    logMsg(logs[logs.length-1])
     if (spawned && username){
       if (message.match(bot.player.username)) {
         bot.chat(`hey ${username}, I'm AFK. Mention me on Discord.`)
@@ -145,7 +155,9 @@ function initBot() {
           bot.chat(`hey ${username}! thanks, I'll be AFK for a while.`)
         }
       }
-      //if (message.match(/[Jj][Oo][Ii][Nn]/))
+      if (message.match(/[Jj][Oo][Ii][Nn][Ee][Dd]/)) {
+        bot.chat(`wb ${username}!`)
+      }
     }
   })
 }
